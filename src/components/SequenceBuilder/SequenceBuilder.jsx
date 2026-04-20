03905ead97a431a9cd43190497fc8be66400491b
Reece Petty <reecejpetty@gmail.com>
2026-04-20 16:56:30 -0500

    Create basic structure of SequenceBuilder component

--- src/components/SequenceBuilder/SequenceBuilder.jsx
+++ src/components/SequenceBuilder/SequenceBuilder.jsx
@@ -1,5 +1,59 @@
-function SequenceBuilder() {
+import styles from './SequenceBuilder.module.css'
 
+function SequenceBuilder() {
+ return (
+  <>
+    <h1>Sequence Builder</h1>
+  </>
+ )
+}
+
+function CurrentSequence() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function KeypressModifiers() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function AddRepeat() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function AddPause() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function StringEntry() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function KeyboardFunctions() {
+  return (
+    <>
+      
+    </>
+  )
 }
 
 export default SequenceBuilder

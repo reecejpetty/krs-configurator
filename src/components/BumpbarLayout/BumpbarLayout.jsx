6a91c103172b7161b9736a592bce1fbe56f58ea2
Reece Petty <reecejpetty@gmail.com>
2026-04-20 15:35:10 -0500

    Add basic Bumpbar Layout styles

--- src/components/BumpbarLayout/BumpbarLayout.jsx
+++ src/components/BumpbarLayout/BumpbarLayout.jsx
@@ -1,5 +1,33 @@
+import styles from './BumpbarLayout.module.css'
+
 function BumpbarLayout() {
-  
+  // User will be able to set rowCount to either 2 or 3
+  const rowCount = 3;
+  const rowArray = Array.from({ length: rowCount });
+  const buttonArray = Array.from({ length: 10 })
+
+  return (
+    <>
+      <h1>Bumpbar Buttons</h1>
+      <div className={styles.bumpbar}>
+        <div className={styles.bumpbarLayout}>
+          {rowArray.map((_, rowIndex) => (
+            buttonArray.map((_, colIndex) => (
+              <BumpbarButton key={(rowIndex * 10) + colIndex} number={(rowIndex * 10) + colIndex + 1} />
+            ))
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+function BumpbarButton({number}) {
+  return (
+    <button className={styles.bumpbarButton} data-number={number}>
+      <span className={styles.bumpbarButtonText}>x</span>
+    </button>
+  )
 }
 
 export default BumpbarLayout

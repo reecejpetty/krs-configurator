692f37126db917e9c03976442a0731129e2fe495
Reece Petty <reecejpetty@gmail.com>
2026-04-20 12:10:02 -0500

    Add additional styles to ConfigOptions component

--- src/components/ConfigOptions.jsx
+++ src/components/ConfigOptions.jsx
@@ -15,7 +15,7 @@ function ConfigOptions() {
 function FileUpload() {
   return (
     <div id="file-upload" className={styles.configSection}>
-      <div id="template-name-entry">
+      <div className={styles.flexRow}>
         <label htmlFor="template-name">Template Name:</label>
         <input type="text" id="template-name" />
       </div>
@@ -27,13 +27,15 @@ function FileUpload() {
 function Connection() {
   return (
     <div id="connection" className={styles.configSection}>
-      <label htmlFor="connection-dropdown">Connection:</label>
-      <select name="connection-dropdown" id="connection-dropdown">
-        <option value="auto">Auto</option>
-        <option value="usb">USB</option>
-        <option value="ps2">PS/2</option>
-        <option value="serial">Serial</option>
-      </select>
+      <div className={styles.flexRow}>
+        <label htmlFor="connection-dropdown">Connection:</label>
+        <select name="connection-dropdown" id="connection-dropdown">
+          <option value="auto">Auto</option>
+          <option value="usb">USB</option>
+          <option value="ps2">PS/2</option>
+          <option value="serial">Serial</option>
+        </select>
+      </div>
     </div>
   )
 }
@@ -41,16 +43,18 @@ function Connection() {
 function Mode() {
   return (
     <div id="mode" className={styles.configSection}>
-      <label htmlFor="mode-dropdown">Mode:</label>
-      <select name="mode-dropdown" id="mode-dropdown">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4" selected>4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-      </select>
+      <div className={styles.flexRow}>
+        <label htmlFor="mode-dropdown">Mode:</label>
+        <select name="mode-dropdown" id="mode-dropdown">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4" selected>4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+        </select>
+      </div>
     </div>
   )
 }
@@ -58,14 +62,20 @@ function Mode() {
 function KeypressSound() {
   return (
     <div id="keypress-sound" className={styles.configSection}>
-      <div id="keypress-sound-options">
+      <div className={styles.flexRow}>
         Keypress Sound:
-        <input type="radio" name="keypress" id="keypress-enable" defaultChecked />
-        <label htmlFor="keypress-enable">Enable</label>
-        <input type="radio" name="keypress" id="keypress-disable" />
-        <label htmlFor="keypress-disable">Disable</label>
+        <div className={styles.flexColumn}>
+          <div className={styles.flexRow}>
+            <input type="radio" name="keypress" id="keypress-enable" defaultChecked />
+            <label htmlFor="keypress-enable">Enable</label>
+          </div>
+          <div className={styles.flexRow}>
+            <input type="radio" name="keypress" id="keypress-disable" />
+            <label htmlFor="keypress-disable">Disable</label>
+          </div>
+        </div>
       </div>
-      <div id="keypress-sound-volume">
+      <div className={styles.flexRow}>
         <label htmlFor="volume-dropdown">Volume:</label>
         <select name="volume-dropdown" id="volume-dropdown">
           <option value="1">1 (Quiet)</option>
@@ -80,22 +90,24 @@ function KeypressSound() {
 function Beeper() {
   return (
     <div id="beeper" className={styles.configSection}>
-      <div>Beeper Sounds On:</div>
-      <div id="beeper-options">
-        <input type="radio" name="beeper-option" id="beeper-never" />
-        <label htmlFor="beeper-never">Never</label>
-        <input type="radio" name="beeper-option" id="beeper-num" />
-        <label htmlFor="beeper-num">Num Lock</label>
-        <input type="radio" name="beeper-option" id="beeper-caps" />
-        <label htmlFor="beeper-caps">Caps Lock</label>
-        <input type="radio" name="beeper-option" id="beeper-scrolls" />
-        <label htmlFor="beeper-scroll">Scroll Lock</label>
-        <input type="radio" name="beeper-option" id="beeper-bel" />
-        <label htmlFor="beeper-bel">BEL</label>
-        <input type="radio" name="beeper-option" id="beeper-other" />
-        <label htmlFor="beeper-other">Other:
-          <input type="text" />
-        </label>
+      <div className={styles.flexRow}>
+        <div>Beeper Sounds On:</div>
+        <div id="beeper-options">
+          <input type="radio" name="beeper-option" id="beeper-never" />
+          <label htmlFor="beeper-never">Never</label>
+          <input type="radio" name="beeper-option" id="beeper-num" />
+          <label htmlFor="beeper-num">Num Lock</label>
+          <input type="radio" name="beeper-option" id="beeper-caps" />
+          <label htmlFor="beeper-caps">Caps Lock</label>
+          <input type="radio" name="beeper-option" id="beeper-scrolls" />
+          <label htmlFor="beeper-scroll">Scroll Lock</label>
+          <input type="radio" name="beeper-option" id="beeper-bel" />
+          <label htmlFor="beeper-bel">BEL</label>
+          <input type="radio" name="beeper-option" id="beeper-other" />
+          <label htmlFor="beeper-other">Other:
+            <input type="text" />
+          </label>
+        </div>
       </div>
     </div>
   )

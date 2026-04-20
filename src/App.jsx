d5cd4bf3bd98b0534edb3c3553f00de416d7ec59
Reece Petty <reecejpetty@gmail.com>
2026-04-20 14:05:57 -0500

    Adjust default React template files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import ConfigOptions from './components/ConfigOptions'
-import BumpbarLayout from './components/BumpbarLayout'
-import ButtonActions from './components/ButtonActions'
-import SequenceBuilder from './components/SequenceBuilder'
+import ConfigOptions from './components/ConfigOptions/ConfigOptions'
+import BumpbarLayout from './components/BumpbarLayout/BumpbarLayout'
+import ButtonActions from './components/ButtonActions/ButtonActions'
+import SequenceBuilder from './components/SequenceBuilder/SequenceBuilder'
 
 function App() {
   return (

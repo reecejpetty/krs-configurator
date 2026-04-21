81307246cab47b656b2eaef089f259bde28c1067
Reece Petty <reecejpetty@gmail.com>
2026-04-21 14:04:54 -0500

    Use Mode 1 layout for demo bumpbar layout

--- src/components/BumpbarLayout/BumpbarLayout.jsx
+++ src/components/BumpbarLayout/BumpbarLayout.jsx
@@ -7,6 +7,7 @@ function BumpbarLayout() {
   const rowCount = activeSwitch;
   const rowArray = Array.from({ length: rowCount });
   const buttonArray = Array.from({ length: 10 })
+  const mode1 = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
 
   return (
     <>
@@ -18,7 +19,7 @@ function BumpbarLayout() {
         <div className={activeSwitch == 2 ? styles.bumpbarLayout20 : styles.bumpbarLayout }>
           {rowArray.map((_, rowIndex) => (
             buttonArray.map((_, colIndex) => (
-              <BumpbarButton key={(rowIndex * 10) + colIndex} number={(rowIndex * 10) + colIndex + 1} />
+              <BumpbarButton key={(rowIndex * 10) + colIndex} number={(rowIndex * 10) + colIndex + 1} text={mode1[(rowIndex * 10) + colIndex] } />
             ))
           ))}
         </div>
@@ -28,10 +29,10 @@ function BumpbarLayout() {
   )
 }
 
-function BumpbarButton({number}) {
+function BumpbarButton({number, text}) {
   return (
     <button className={styles.bumpbarButton} data-number={number}>
-      <span className={styles.bumpbarButtonText}>x</span>
+      <span className={styles.bumpbarButtonText}>{text}</span>
     </button>
   )
 }

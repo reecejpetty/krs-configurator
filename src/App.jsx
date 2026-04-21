4a439f972c988e21e9a45caf960690b3a28ca3d5
Reece Petty <reecejpetty@gmail.com>
2026-04-21 11:36:09 -0500

    Remove unnecessary import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 import ConfigOptions from './components/ConfigOptions/ConfigOptions'
 import BumpbarLayout from './components/BumpbarLayout/BumpbarLayout'
-import ButtonActions from './components/ButtonActions/ButtonActions'
 import SequenceBuilder from './components/SequenceBuilder/SequenceBuilder'
 
 function App() {

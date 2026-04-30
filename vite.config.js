92ebb99576e68bccf3372c6bf37c3c833718009c
Reece Petty <reecejpetty@gmail.com>
2026-04-30 10:48:15 -0500

    Add PWA functionality

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "KRS Online Configurator",
+        "short_name": "Configurator",
+        "start_url": "/krs-configurator/",
+        "display": "standalone",
+        "background_color": "#F7F8FA",
+        "theme_color": "#1A3F6F",
+        "icons": [
+          {
+            "src": "mobile_logo_192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "mobile_logo.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
+  ],
   base: "/krs-configurator/"
 })
